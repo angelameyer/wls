62c776ee12053aa5521c4a61eb645fa2b0fd3f67
Angela Meyer <angela.td.meyer@gmail.com>
2014-03-23 17:08:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wls::Application.config.secret_key_base = '04254599748237844d96abeecebb9e6eff00ba08c0480f70d8f8dbde01ed8cb97897a4b01a92de41a2d34dc569b7d8b8e66b753f26039155d775b15c2582bedc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wls::Application.config.session_store :cookie_store, key: '_wls_session'
